e88c7ff34bf066e4ea1b00837371802a0af56f0b
Roger Bumgarner <neoice@neoice.net>
2012-05-28 20:04:36 -0700

    included test script for HTTP POSTing.

---
+++ test.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+
+var options = {
+	host: 'localhost',
+	port: 3002,
+	path: '/user/find/neoice@neoice.net',
+	method: 'GET',
+};
+
+var soa_req = http.request(options, function(soa_res) {
+        console.log('STATUS: ' + soa_res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(soa_res.headers));
+        //soa_res.setEncoding('utf8');
+
+	soa_res.on('data', function (chunk) {
+		console.log('BODY: ' + chunk);
+	});
+
+});
+
+//soa_req.write(req.files.file.path.toString());
+soa_req.end();
+
+
